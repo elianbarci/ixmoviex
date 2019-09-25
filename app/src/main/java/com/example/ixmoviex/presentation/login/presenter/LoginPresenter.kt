75427245dc04877c945955d75b0a0aeb65328214
elianbarci <eli.barci@gmail.com>
2019-09-25 17:58:04 -0300

    Login: parte 2

--- app/src/main/java/com/example/ixmoviex/presentation/login/presenter/LoginPresenter.kt
+++ app/src/main/java/com/example/ixmoviex/presentation/login/presenter/LoginPresenter.kt
@@ -1,11 +1,18 @@
 package com.example.ixmoviex.presentation.login.presenter
 //Claramente este es el presenter, la logica que cumple el presener es muy simple no màs que
 //chequear campos vacios, u homologar que los datos ingresados se hayan cargado correctamente.
+import com.example.ixmoviex.domain.interactor.logininteractor.SignInInteractor
 import com.example.ixmoviex.presentation.login.LoginContract
+import kotlin.math.sign
 
-class LoginPresenter() : LoginContract.LoginPresenter {
+class LoginPresenter(signInInteractor: SignInInteractor) : LoginContract.LoginPresenter {   //Inyectamos la interfaz que vamos a utilizar
 
     var view: LoginContract.LoginView? = null
+    var signInInteractor: SignInInteractor? = null      //Se inicializa el interactor
+
+    init {
+        this.signInInteractor = signInInteractor        //Ese signInterac
+    }
 
     override fun attachView(view: LoginContract.LoginView) {
         this.view = view
@@ -20,8 +27,25 @@ class LoginPresenter() : LoginContract.LoginPresenter {
     }
 
     override fun signInUserWithEmailAndPassword(email: String, password: String) {
-        view?.showProgressBar()
-        view?.showError("Hasta acá todo bien, llego al presenter")
+
+        signInInteractor?.signInWithEmailAndPassword(email,password, object: SignInInteractor.SigninCallback{        //Puedo pasar interfacez en las funciones siempre y cuando se implemente
+            override fun onSignInSuccess() {
+                if(isViewAttached()){
+                    view?.showProgressBar()
+                    view?.navigateToMain()
+                }
+            }
+
+            override fun onSignInFailure(errorMsg: String) {
+                if(isViewAttached()){
+                    view?.showProgressBar()
+                    view?.showError(errorMsg)
+                }
+            }
+
+        })
+
+
 
     }
 
